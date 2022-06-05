702980e0e795a61a5cc483d9f3e3aa7322739ea7
Влад <vlad@MacBook-Pro-Vlad.local>
2022-06-05 12:04:55 +0300

    add new file

---
+++ script.js
@@ -0,0 +1,10 @@
+R = 10
+H = 20
+S = Math.PI * R * R
+V = S * H
+console.log ("**************")
+console.log("Обьем цилиндра с площадью основы S = ",S,", радиусом R =",R,"и высотой H = ",H,"равен:"  ) 
+console.log("-------------------");
+console.log("V =",V);
+console.log("-------------------");
+console.log("end.");
